a60638f5d7af827fa64d29ca4ae4a7c15401793a
Suraj <surajgautam1111@gmai;l.com>
2023-03-17 23:12:18 +0545

    loading and spinner added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import NavBar from './components/NavBar';
 
 import React, { Component } from 'react'
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
        <NavBar/>
+       <News pageSize={5}/>
       </div>
     )
   }
