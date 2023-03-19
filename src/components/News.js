109ba52cff6be4b412c75cc0819234e7973ecd21
Suraj <surajgautam1111@gmai;l.com>
2023-03-19 23:00:28 +0545

    news app Completed

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from "./Spinner.js";
+import PropTypes from 'prop-types'
+
 export class News extends Component {
+static defaultProps={
+  country:'us',
+  pageSize:8,
+  category:'general'
+}  
 
-
-  
+static propTypes={
+  country:PropTypes.string,
+  pageSize:PropTypes.number,
+  category:PropTypes.string
+}
   constructor(){
     super();
     this.state={
@@ -15,8 +25,8 @@ export class News extends Component {
     }
   }
    async componentDidMount(){
-    
-    let url=`https://newsapi.org/v2/everything?q=tesla&from=2023-02-17&sortBy=publishedAt&apiKey=dcce376f0e194fdca5bb7630a3199741&page=1&pageSize=${this.props.pageSize}`
+            //https://newsapi.org/v2/top-headlines?country={this.props.country}us&category=${this.props.category}&apiKey=dcce376f0e194fdca5bb7630a3199741
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dcce376f0e194fdca5bb7630a3199741&page=1&pageSize=${this.props.pageSize}`
     this.setState({loading:true})
     let data= await fetch(url)
     let parsedData= await data.json()
@@ -31,7 +41,7 @@ export class News extends Component {
   }
   handlePrevClick =async ()=>{
 console.log("handle Prev")
-let url=`https://newsapi.org/v2/everything?q=tesla&from=2023-02-17&sortBy=publishedAt&apiKey=dcce376f0e194fdca5bb7630a3199741&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dcce376f0e194fdca5bb7630a3199741&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
 this.setState({loading:true})    
 let data= await fetch(url)
     let parsedData= await data.json()
@@ -46,7 +56,7 @@ let data= await fetch(url)
 console.log("handle next")
 if(!(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)))
     {
-      let url=`https://newsapi.org/v2/everything?q=tesla&from=2023-02-17&sortBy=publishedAt&apiKey=dcce376f0e194fdca5bb7630a3199741&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+      let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dcce376f0e194fdca5bb7630a3199741&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
       this.setState({loading:true})
       let data= await fetch(url)
       let parsedData= await data.json()
@@ -71,7 +81,7 @@ if(!(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)))
         return(
         
           <div className="col-md-4" key={element.url}>
-          <NewsItem  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+          <NewsItem  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
               </div>
          
           
